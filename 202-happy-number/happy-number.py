7615df75e47b93acbbaea29ec99a0000dad17b88
Anuja Merwade <117044996+anujamerwade@users.noreply.github.com>
2023-11-24 18:59:18 -0500

    Time: 42 ms (58.84%) | Memory: 16.2 MB (41.88%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,18 +1,27 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
 
-        fast, slow = n, n
+        visited = set()
+        ans = n
 
-        # slow = self.findSquares(slow)
-        # fast = self.findSquares(self.findSquares(fast))
-        
         while True:
-            slow = self.findSquares(slow)
-            fast = self.findSquares(self.findSquares(fast))
-            if slow == fast:
-                break
+            ans = self.findSquares(ans)
+            if ans == 1:
+                return True
+            if ans not in visited:
+                visited.add(ans)
+            else:
+                return False
+
+        # fast, slow = n, n
+        
+        # while True:
+        #     slow = self.findSquares(slow)
+        #     fast = self.findSquares(self.findSquares(fast))
+        #     if slow == fast:
+        #         break
   
-        return slow == 1
+        # return slow == 1
 
     
     def findSquares(self, num):
