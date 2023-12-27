ebaa92dde35e8bd9e8ac8122743cf838db8532d0
Anuja Merwade <117044996+anujamerwade@users.noreply.github.com>
2023-12-27 13:13:36 -0500

    Time: 34 ms (81.76%) | Memory: 17.1 MB (6.80%) - LeetSync

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        c = 0
+        return self.helper(num, c)
+        
+    def helper(self, num, c):
+        if num == 0:
+            return c
+        if num % 2 == 0:
+            # even number
+            num /= 2
+            c += 1
+            return self.helper(num, c)
+        else:
+            num -= 1
+            c += 1
+            return self.helper(num, c)
+
+        
