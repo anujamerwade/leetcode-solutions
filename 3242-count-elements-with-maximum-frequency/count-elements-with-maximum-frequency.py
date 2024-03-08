aaf740da6199ea7a4cb91c44cb9f697dbc0ebedd
Anuja Merwade <117044996+anujamerwade@users.noreply.github.com>
2024-03-07 19:22:39 -0500

    Time: 42 ms (69.12%) | Memory: 16.4 MB (97.35%) - LeetSync

---
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        hashmap = {}
+
+        for num in nums:
+            hashmap[num] = hashmap.get(num, 0) + 1
+        
+        val_list = list(hashmap.values())
+        max_freq = max(val_list)
+
+        ans = 0
+
+        for n in val_list:
+            if n == max_freq:
+                ans += max_freq
+        return ans
